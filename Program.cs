4f3cdddcfa29eead152472862563b1b432d5cfc7
FIC <lucas_redigolo@hotmail.com>
2017-10-17 19:11:44 -0200

    comentarios sobre a aula

--- Program.cs
+++ Program.cs
@@ -8,11 +8,23 @@ namespace DesviodeFluxo
         {
 
             int salario = 1000;
-            while (salario<5000){
+            while (salario<5000){ // repete o comando abaixo sempre que for TRUE
 
-                salario*=100;
+                salario*=100; // *= substitui "salario = salario * 100"
                 System.Console.WriteLine("Meu salário ainda é de " + salario);
             }    
+
+            int valor =0;
+
+            System.Console.WriteLine("Digite um numero para ver sua tabuada:");
+            valor = Int16.Parse(Console.ReadLine()); // parse usado para converter para a variavel desejada (por default um comando READ sempre le string)
+
+            int i;
+            for (i = 0; i <= 10; i++)
+            {
+               System.Console.WriteLine(valor + " x " + i + " = " +(valor * i)); 
+            }
+
 
             }
         }
